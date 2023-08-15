85416e3da7e7d9ec3cf64aa5c6548004a916778d
OBLEMOS <zaim1oss14@gmail.com>
2023-08-15 14:06:19 +0100

    getting ready to push an exemple cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,19 @@
-## The first function makeCacheMatrix creates a special matrix object and allows the following operations: setting and getting the value of the matrix+setting and getting the value of the inverse
-## The second function cacheSolve returns the cached value of the inverse if it exists,if not the function calculates the inverse itself
+## The first function .makeCacheMatrix creates a special matrix object and allows the following operations: setting and getting the value of the matrix+setting and getting the value of the inverse
+## The second function .cacheSolve returns the cached value of the inverse if it exists,if not the function calculates the inverse itself
 makeCacheMatrix <- function(x = matrix()) {
-    inv<-NULL
-    
-    set_mat<-function(M){   x<<-M
-                            inv<<-NULL
-    }
-    
-    get_mat<-function() x
-    
-    set_inv<-function(inverse) inv<<-inverse
-    
-    get_inv<-function() inv
-    
-    
+  inv<-NULL
+  
+  set_mat<-function(M){   x<<-M
+  inv<<-NULL
+  }
+  
+  get_mat<-function() x
+  
+  set_inv<-function(inverse) inv<<-inverse
+  
+  get_inv<-function() inv
+  
+  
   list(set_mat=set_mat,get_mat=get_mat,set_inv=set_inv,get_inv=get_inv)
   
 }
@@ -21,17 +21,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x,...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   inv<-x$get_inv()
   
   if(!is.null(inv)){
     
     return(inv)
   }
-    mat<-x$get_mat
-    inv<-solve(mat,...)
-    x$set_inv(inv)
-    inv
+  mat<-x$get_mat
+  inv<-solve(mat,...)
+  x$set_inv(inv)
+  inv
   
   
 }
